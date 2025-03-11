13a710fc31c3181abc6d19d0da299273b43ac050
bkahuy <buikhachuy003@gmail.com>
2025-03-12 01:03:20 +0700

    backend index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,19 @@
-import { serve } from "@hono/node-server";
-import app from "./app";
+import { Hono } from "hono";
+import { dotDoAnRoutes } from "./src/routes/dotDoAnRoutes";
+import { dotThucTapRoutes } from "./src/routes/dotThucTapRoutes";
+import { doAnRoutes } from "./src/routes/doAnRoutes";
+import { congTyRoutes } from "./src/routes/congTyRoutes";
 
-serve({
-  fetch: app.fetch,
+const app = new Hono();
+
+// Đăng ký các route vào app
+app.route("/dot-do-an", dotDoAnRoutes);
+app.route("/dot-thuc-tap", dotThucTapRoutes);
+app.route("/do-an", doAnRoutes);
+app.route("/cong-ty", congTyRoutes);
+
+// Khởi động server trên port 3000
+export default {
   port: 3000,
-});
-
-console.log("Server đang chạy tại http://localhost:3000");
+  fetch: app.fetch,
+};
